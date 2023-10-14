46c0fa7999bbab320f73d4030f640048cc9d3a17
Your Name <you@example.com>
2023-10-14 20:25:24 +0300

    some edits on dimensions

--- src/Components/Brands.js
+++ src/Components/Brands.js
@@ -1,34 +1,46 @@
 import { Grid, Sheet } from "@mui/joy";
+import { styled } from "@mui/joy/styles";
+
+const Image = styled("img")(({ theme }) => ({
+  minWidth: "135px",
+  padding: "0",
+}));
 
 export const Brands = () => {
   return (
     <>
-      <Sheet sx={{ padding: "0 20px", background: "var(--bright)" }}>
+      <Sheet
+        sx={{
+          padding: "0 20px",
+          background: "var(--bright)",
+        }}>
         <h2>Shop by Brands</h2>
       </Sheet>
 
       <Grid
         container
         columns={{ lg: 2, sm: 3 }}
-        spacing={{ xs: 2, md: 3 }}
-        sx={{ padding: "20px" }}>
+        spacing={{ xs: 1, md: 3 }}
+        sx={{
+          padding: "20px",
+        }}>
         <Grid>
-          <img src={require(`../Assets/zara.png`)} />
+          <Image src={require(`../Assets/zara.png`)} />
         </Grid>
         <Grid>
-          <img src={require(`../Assets/D&G.png`)} />
+          <Image src={require(`../Assets/D&G.png`)} />
         </Grid>
         <Grid>
-          <img src={require(`../Assets/H&M.png`)} />
+          <Image src={require(`../Assets/H&M.png`)} />
         </Grid>
         <Grid>
-          <img src={require(`../Assets/chanel.png`)} />
+          <Image src={require(`../Assets/chanel.png`)} />
         </Grid>
         <Grid>
-          <img src={require(`../Assets/prada.png`)} />
+          <Image src={require(`../Assets/prada.png`)} />
         </Grid>
         <Grid>
-          <img src={require(`../Assets/biba.png`)} />
+          <Image src={require(`../Assets/biba.png`)} />
         </Grid>
       </Grid>
     </>
