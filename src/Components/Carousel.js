c978d784b3b020d79a80a1a0f3da5f036e069186
Your Name <you@example.com>
2023-11-06 12:47:26 +0200

    make the carousel and three cards clickable

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -4,6 +4,7 @@ import { useContext, useState } from "react";
 import AutoPlaySwipeableViews from "react-swipeable-views-react-18-fix";
 import SlidesGenerator from "./SlidesGenerator";
 import SlidesContext from "../Shared/Context/SlidesContext";
+import { Link } from "react-router-dom";
 
 const Carousel = () => {
   const theme = useTheme();
@@ -19,29 +20,36 @@ const Carousel = () => {
   };
 
   return (
-    <Box sx={{ flexGrow: 1, 
-      mb: {
-      xs: 6,
-      sm: 6,
-      md: 10,
-      lg: 8,
-      xl: 7,
-    },
-      height: {
-      xs: 180,
-      sm: 270,
-      md: 320,
-      lg: 420,
-      xl: 420,
-    }, }}>
-      <AutoPlaySwipeableViews
-        axis={theme.direction === "ltr" ? "x-reverse" : "x"}
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents>
-        {SlidesGenerator()}
-      </AutoPlaySwipeableViews>
-    </Box>
+    <>
+      <Link to={`/category?categoryName=handbags`}>
+        <Box
+          sx={{
+            flexGrow: 1,
+            mb: {
+              xs: 6,
+              sm: 6,
+              md: 10,
+              lg: 8,
+              xl: 7,
+            },
+            height: {
+              xs: 180,
+              sm: 270,
+              md: 320,
+              lg: 420,
+              xl: 420,
+            },
+          }}>
+          <AutoPlaySwipeableViews
+            axis={theme.direction === "ltr" ? "x-reverse" : "x"}
+            index={activeStep}
+            onChangeIndex={handleStepChange}
+            enableMouseEvents>
+            {SlidesGenerator()}
+          </AutoPlaySwipeableViews>
+        </Box>
+      </Link>
+    </>
   );
 };
 
